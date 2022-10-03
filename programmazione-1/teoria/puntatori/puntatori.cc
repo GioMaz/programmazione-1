385440998b87a3a9f0a3c73d1fd031691fda5b90
giovmaz <giovmaz2003@gmail.com>
2022-10-03 13:08:54 +0200

    removed prove

---
+++ programmazione-1/teoria/puntatori/puntatori.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 1;      // a punta 0x16b60f5cc
+
+    // Riferimento
+    int &b = a;     // b punta 0x16b60f5cc
+
+    // Puntatore
+    int *pa = &a;   // pa punta 0x16b60f5cc
+    int c = *pa;    // c punta 0x16af575b4
+}
