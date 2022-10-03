3726e44efb1b2a94e7de18118c34f4081e5ed7ee
giovmaz <giovmaz2003@gmail.com>
2022-10-03 13:23:57 +0200

    puntatori void

--- programmazione-1/teoria/puntatori/puntatori.cc
+++ programmazione-1/teoria/puntatori/puntatori.cc
@@ -12,4 +12,7 @@ int main() {
     // Puntatore
     int *pa = &a;   // pa punta 0x16b60f5cc
     int c = *pa;    // c punta 0x16af575b4
+
+    // Puntatore void
+    void *pa1 = pa; // pa1 punta 0x16b60f5cc
 }
