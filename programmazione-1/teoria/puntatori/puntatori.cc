b398dd14a7b1f47b3b6cfc58af38d1283c5ad564
giovmaz <giovmaz2003@gmail.com>
2022-10-10 12:36:20 +0200

    parametri funzioni

--- programmazione-1/teoria/puntatori/puntatori.cc
+++ programmazione-1/teoria/puntatori/puntatori.cc
@@ -3,16 +3,40 @@
 
 using namespace std;
 
+void valore(int a);
+void riferimento(int &a);
+void puntatore(int *a);
+
 int main() {
     int a = 1;      // a punta 0x16b60f5cc
 
+    // Valore (copia)
+    int b = a;
+
     // Riferimento
-    int &b = a;     // b punta 0x16b60f5cc
+    int &c = a;     // b punta 0x16b60f5cc
 
     // Puntatore
-    int *pa = &a;   // pa punta 0x16b60f5cc
-    int c = *pa;    // c punta 0x16af575b4
+    int *pa = &a;   // pa vale 0x16b60f5cc
+    int d = *pa;    // c punta 0x16af575b4
+
+    // Funzioni
+    valore(a);
+    riferimento(a);
+    puntatore(&a);
 
     // Puntatore void
     void *pa1 = pa; // pa1 punta 0x16b60f5cc
+}
+
+void valore(int a) {
+    cout << a << endl;
+}
+
+void riferimento(int &a) {
+    cout << a << endl;
+}
+
+void puntatore(int *a) {
+    cout << *a << endl;
 }
