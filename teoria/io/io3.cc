f9cf4d13cd35426b576f92978a3c897ed71f5718
giovmaz <giovmaz2003@gmail.com>
2022-11-15 18:47:28 +0100

    removed librerie

--- teoria/io/io3.cc
+++ teoria/io/io3.cc
@@ -8,20 +8,23 @@ int main() {
 
     // file << "Ciao"
     out.open("testo.txt", ios::out);
+    if (out) cout << "File esiste!" << endl;
     out << "Ciao";
     out.close();
 
     // file >> buffer
     in.open("testo.txt", ios::in);
+    if (in) cout << "File esiste!" << endl;
     char buffer[5];
     in >> buffer;
     // oppure
-    // in.getline(buffer, 5);
+    // in.getline(buffer, 500);
     in.close();
     cout << buffer << endl;
 
     // file << "Ciao"
     append.open("testo.txt", ios::out|ios::app);
+    if (app) cout << "File esiste!" << endl;
     append.close();
 
     return 0;
