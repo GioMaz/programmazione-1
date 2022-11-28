7ca870d34b13b59ecff44667d18555af311054e4
giovmaz <giovmaz2003@gmail.com>
2022-11-28 11:57:46 +0100

    error check when opening file

--- teoria/io/io3.cc
+++ teoria/io/io3.cc
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
-    fstream in, out, append;
+    fstream in, out, app;
 
     // file << "Ciao"
     out.open("testo.txt", ios::out);
+    if (out.fail()) exit(1);
     if (out) cout << "File esiste!" << endl;
     out << "Ciao";
     out.close();
 
     // file >> buffer
     in.open("testo.txt", ios::in);
+    if (in.fail()) exit(1);
     if (in) cout << "File esiste!" << endl;
     char buffer[5];
     in >> buffer;
     // oppure
-    // in.getline(buffer, 500);
+    // in.getline(buffer, 5);
     in.close();
-    cout << buffer << endl;
 
     // file << "Ciao"
-    append.open("testo.txt", ios::out|ios::app);
+    app.open("testo.txt", ios::out|ios::app);
     if (app) cout << "File esiste!" << endl;
-    append.close();
+    app.close();
 
     return 0;
 }
