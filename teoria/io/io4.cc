33cd01329d2ac41d21c2ff0ed4fa46d005ac0cf2
giovmaz <giovmaz2003@gmail.com>
2022-12-01 15:42:16 +0100

    io4 argc check

--- teoria/io/io4.cc
+++ teoria/io/io4.cc
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout << "Sono richiesti almeno 2 argomenti";
+        return 1;
+    }
     for (int i = 0; i < argc; i++) {
         cout << argv[i] << endl;
     }
