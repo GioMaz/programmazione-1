4de26889d0e9f7c93c6c6a70b53173224f78f4e0
giovmaz <giovmaz2003@gmail.com>
2023-01-19 12:39:22 +0100

    base

---
+++ esercizi/base/base.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+bool from_decimal(int n, int base, char res[]) {
+    bool result = true;
+
+    if (base > 16) { // BASE 16
+        result = false;
+    }
+
+    char digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' }; // BASE 16
+    int max_len = 100;
+
+    int n1 = n;
+    int base1 = base;
+
+    char res1[max_len];
+    int i = max_len - 1;
+    while (n1 != 0 && i >= 0) {
+        res1[i] = digits[n1 % base1];
+        n1 /= base1;
+        i--;
+    }
+
+    i++;
+    for (int j = 0; j < max_len - i; j++) {
+        res[j] = res1[i + j];
+    }
+    res[max_len - i] = '\0';
+
+    return result;
+}
+
+bool to_decimal(char n[], int base, int &res) {
+    bool result = true;
+
+    if (base > 16) { // BASE 16
+        result = false;
+    }
+
+    int max_len = 100;
+
+    int i = 0;
+    int cur = 1;
+    int res1[max_len];
+    while (n[i] != '\0') {
+        if (n[i] >= 'A' && n[i] <= 'F') { // BASE 16
+            cur = 10 + n[i] - 'A';
+        }
+        else if (n[i] >= '0' && n[i] <= '9'){
+            cur = n[i] - '0';
+        }
+        else {
+            result = false;
+        }
+
+        res1[i] = cur;
+
+        i++;
+    }
+
+    int index = i - 1;
+    for (int j = 0; j < i; j++) {
+        int pow = 1;
+        for (int k = 1; k <= index; k++) {
+            pow *= base;
+        }
+        res += res1[j]*pow;
+        index--;
+    }
+
+    return result;
+}
+
+int main() {
+    char res[100];
+    from_decimal(455951, 14, res);
+    cout << res << endl;
+
+    int n;
+    to_decimal(res, 14, n);
+    cout << n << endl;
+
+    return 0;
+}
